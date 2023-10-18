802540b2ff916e6dfc38e1bd61b7d95541cce1cb
Corexproga <62663778+Corexproga@users.noreply.github.com>
2023-10-18 19:52:48 +0200

    Kobold AI, aggro script

---
+++ Assets/Scripts/Kobold.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Kobold : MonoBehaviour
+{
+    public float speed = 5f;
+
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+
+    }
+}
